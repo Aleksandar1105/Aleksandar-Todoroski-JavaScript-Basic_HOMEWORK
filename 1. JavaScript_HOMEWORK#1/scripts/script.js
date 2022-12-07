79ebf8c8f34d335f8958a62fdde94abe17388b78
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2022-12-07 22:26:01 +0100

    Added Homework#1

---
+++ 1. JavaScript_HOMEWORK#1/scripts/script.js
@@ -0,0 +1,13 @@
+/* Write a JavaScript program that will calculate the price of 30 
+Phones, where the price of one phone is $119.95 and the tax rate is 5%. */
+
+const phonePrice = 139.95;
+const taxRate = 0.05;
+const phoneTotalPrice = phonePrice + phonePrice * taxRate;
+
+const numberOfPhones = prompt('Please enter the number of phones:');
+const convertedNumberOfPhones = parseFloat(numberOfPhones);
+
+console.log('Total price for your input is: ' + '$', convertedNumberOfPhones * phoneTotalPrice);
+
+
