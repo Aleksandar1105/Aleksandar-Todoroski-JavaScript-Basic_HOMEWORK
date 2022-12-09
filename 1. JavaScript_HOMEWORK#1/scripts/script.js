f7a1fe15e4c17a202e107701194098250c38f921
Aleksandar <109741787+Aleksandar1105@users.noreply.github.com>
2022-12-09 12:21:49 +0100

    Update script.js

--- 1. JavaScript_HOMEWORK#1/scripts/script.js
+++ 1. JavaScript_HOMEWORK#1/scripts/script.js
@@ -1,7 +1,7 @@
 /* Write a JavaScript program that will calculate the price of 30 
 Phones, where the price of one phone is $119.95 and the tax rate is 5%. */
 
-const phonePrice = 139.95;
+const phonePrice = 119.95;
 const taxRate = 0.05;
 const phoneTotalPrice = phonePrice + phonePrice * taxRate;
 
