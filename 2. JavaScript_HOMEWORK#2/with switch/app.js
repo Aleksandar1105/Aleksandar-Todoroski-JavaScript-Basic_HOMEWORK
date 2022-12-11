58fe456e2c8550a33ccd2257c425b45128af8e00
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2022-12-11 01:25:44 +0100

    Homework 2 - with switch

---
+++ 2. JavaScript_HOMEWORK#2/with switch/app.js
@@ -0,0 +1,47 @@
+const year = prompt('Please enter your birth year:');
+const parsedYear = parseInt(year);
+
+const zodiacCalculation = (parsedYear - 4) % 12;
+
+switch (zodiacCalculation) {
+    case 0:
+        console.log('Rat');
+        break;
+    case 1:
+        console.log('Ox');
+        break;
+    case 2:
+        console.log('Tiger');
+        break;
+    case 3:
+        console.log('Rabbit');
+        break;
+    case 4:
+        console.log('Dragon');
+        break;
+    case 5:
+        console.log('Snake');
+        break;
+    case 6:
+        console.log('Horse');
+        break;
+    case 7:
+        console.log('Goat');
+        break;
+    case 8:
+        console.log('Monkey');
+        break;
+    case 9:
+        console.log('Rooster');
+        break;
+    case 10:
+        console.log('Dog');
+        break;
+    case 11:
+        console.log('Pig');
+        break;
+    default:
+        console.log(`You've entered an invalid input! \nYour input must contain only whole numbers!`);
+        break;
+}
+
