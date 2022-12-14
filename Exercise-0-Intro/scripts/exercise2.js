11a9a79e18b78f505a02217a12dcd65209a490fd
Aleksandar <109741787+Aleksandar1105@users.noreply.github.com>
2022-12-14 13:49:39 +0100

    Delete Exercise-0-Intro directory

--- Exercise-0-Intro/scripts/exercise2.js
+++
@@ -1,9 +0,0 @@
-/* Write a JavaScript program that will read the two sides of a rectangle 
-and calculate its area
-TIP: area = a * b */
-
-let sideA = parseFloat(prompt('Enter the value for side a:'));
-let sideB = parseFloat(prompt('Enter the value for side b:'));
-
-let area = sideA * sideB;
-console.log('The area of rectungle is:', area);
