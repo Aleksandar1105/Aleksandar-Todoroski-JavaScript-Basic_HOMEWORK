78902b3d61380bcf17fba9bad5e5490974ce1bf0
Aleksandar <109741787+Aleksandar1105@users.noreply.github.com>
2022-12-15 10:27:26 +0100

    Update script.js

--- --Exercise-3_Numbers in Array/script.js
+++ --Exercise-3_Numbers in Array/script.js
@@ -2,6 +2,7 @@ const numArray = [3, 5, 8, 3, 6, 11, 35, 34, 22, 11, 4, 2, 3];
 
 function findNumber(number, array) {
     let countNumber = 0;
+
     for (let i = 0; i < array.length; i++) {
         if (number === array[i]) {
             countNumber += 1;
@@ -10,4 +11,8 @@ function findNumber(number, array) {
     return countNumber;
 }
 
-console.log(findNumber(3, numArray));
+const inputNumber = prompt('Please enter number:');
+const convertedNumber = Number(inputNumber);
+
+console.log('There is', findNumber(convertedNumber, numArray), 'occurrences of number', convertedNumber, 'in the array');
+
