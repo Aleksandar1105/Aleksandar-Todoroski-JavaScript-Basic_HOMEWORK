46d141ede41c29f2a701197e0992e239f481fe17
Aleksandar <109741787+Aleksandar1105@users.noreply.github.com>
2022-12-23 14:14:46 +0100

    Update script.js

--- 6. JavaScript_HOMEWORK#6/script.js
+++ 6. JavaScript_HOMEWORK#6/script.js
@@ -5,10 +5,10 @@
     - In every table cell print which row and column it is (ex. Row-3 Column-1)
     - Don't forget to use google! :) */
 
-const tableBody = document.querySelector('#table-body');
 const button = document.querySelector('#create-table');
 
 button.addEventListener("click", function clickToCreate() {
+    const tableBody = document.querySelector('#table-body');
     const rowsNum = parseInt(prompt('Enter number of rows:'));
     const columnsNum = parseInt(prompt('Enter number of columns'));
 
